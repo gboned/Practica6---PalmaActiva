10e34dca6c6b2ff463f06c89319194ffd03ef0df
gboned <gboned@cifpfbmoll.eu>
2020-05-18 12:19:16 +0200

    Añadir presentación de la tarea

---
+++ js/funciones.js
@@ -0,0 +1,10 @@
+/**************************************      
+ *                                    /
+ *     MARCAS DE COCHE DEL USUARIO    /
+ *                                    /
+ *************************************/
+
+ /** 
+  * Esta aplicación pide al usuario su nombre y tres marcas de 
+  * coche que ha tenido y almacena las marcas de coche en una array.
+  */

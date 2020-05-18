8a61b6c65ec6659366fe0042877c48d7f884c3b3
gboned <gboned@cifpfbmoll.eu>
2020-05-18 13:08:43 +0200

    Añadir defaults a los prompts

--- js/funciones.js
+++ js/funciones.js
@@ -16,19 +16,19 @@ var listaMarcasDeCoche = [];
 function solicitarDatosUsuario() {
 
     // Solicito el nombre del usuario mediante un prompt, que se guarda en una variable.
-    var nombreUsuario = String(prompt("Por favor, introduce tu nombre: "));
+    var nombreUsuario = String(prompt("Por favor, introduce tu nombre: ", "Nombre"));
 
     // Saludo al usuario por su nombre y le pido que introduzca su primera marca de coche.
-    var marcaCoche1 = String(prompt(`Hola ${nombreUsuario}, introduce tu primera marca de coche: `));
+    var marcaCoche1 = String(prompt(`Hola ${nombreUsuario}, introduce tu primera marca de coche: `, "Primera marca de coche"));
         // Añado la primera marca de coche al array de la variable global.
         listaMarcasDeCoche.push(marcaCoche1);
 
     // Le pido al usuario la segunda marca de coche, y también la almaceno en la array.
-    var marcaCoche2 = String(prompt("Introduce tu segunda marca de coche: "));
+    var marcaCoche2 = String(prompt("Introduce tu segunda marca de coche: ", "Segunda marca de coche"));
         listaMarcasDeCoche.push(marcaCoche2);
 
     // Le pido al usuario la tercera marca de coche, y la guardo en la array.
-    var marcaCoche3 = String(prompt("Introduce tu tercera marca de coche: "))
+    var marcaCoche3 = String(prompt("Introduce tu tercera marca de coche: ", "Tercera marca de coche"))
         listaMarcasDeCoche.push(marcaCoche3);
 
         // Muestro en la consola la array resultante.

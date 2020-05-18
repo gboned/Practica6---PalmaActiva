4d7e53267da1305601a4b8ba8c49f982ad7d0031
gboned <gboned@cifpfbmoll.eu>
2020-05-18 12:56:10 +0200

    Añadir comentarios y mejorar estructura del código

--- js/funciones.js
+++ js/funciones.js
@@ -14,13 +14,24 @@ var listaMarcasDeCoche = [];
 
 // Creo una función que solicite al usuario su nombre y las 3 marcas de coche.
 function solicitarDatosUsuario() {
+
+    // Solicito el nombre del usuario mediante un prompt, que se guarda en una variable.
     var nombreUsuario = String(prompt("Por favor, introduce tu nombre: "));
+
+    // Saludo al usuario por su nombre y le pido que introduzca su primera marca de coche.
     var marcaCoche1 = String(prompt(`Hola ${nombreUsuario}, introduce tu primera marca de coche: `));
+        // Añado la primera marca de coche al array de la variable global.
         listaMarcasDeCoche.push(marcaCoche1);
+
+    // Le pido al usuario la segunda marca de coche, y también la almaceno en la array.
     var marcaCoche2 = String(prompt("Introduce tu segunda marca de coche: "));
         listaMarcasDeCoche.push(marcaCoche2);
+
+    // Le pido al usuario la tercera marca de coche, y la guardo en la array.
     var marcaCoche3 = String(prompt("Introduce tu tercera marca de coche: "))
         listaMarcasDeCoche.push(marcaCoche3);
+        
+        // Muestro en la consola la array resultante.
         console.log(listaMarcasDeCoche);
 }
 
